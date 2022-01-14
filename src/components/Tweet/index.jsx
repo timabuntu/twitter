a616ccc7ff82254daa29370fcb58c76dd8087404
thiago lopes de mello <timabuntu@gmail.com>
2022-01-14 10:21:22 -0300

    feat: main tweet created

--- src/components/Tweet/index.jsx
+++ src/components/Tweet/index.jsx
@@ -1,32 +1,62 @@
 import { IoEllipsisHorizontalSharp as IEtc } from 'react-icons/io5';
+import PostImg from '../../assets/images/tailwind_3.jpeg';
+
 const Tweet = () => {
   return (
-    <section className="p-4 mt-1 border-b-2 border-gray-100 cursor-pointer hover:bg-gray-100">
-      <article>
-        <section className="flex">
-          <div className="flex bg-green-200">
-            <img
-              className="w-12 h-12 rounded-full"
-              src="https://randomuser.me/api/portraits/men/26.jpg"
-              alt="user-profile"
-            />
-          </div>
+    <article className="p-4 flex border-b-2 w-[600px] border-gray-100 cursor-pointer hover:bg-gray-100">
+      <img
+        className="w-12 h-12 rounded-full"
+        src="https://randomuser.me/api/portraits/men/26.jpg"
+        alt="user-profile"
+      />
 
-          <div className="flex justify-between w-full">
-            <div className="flex-1">
-              <div className="flex ml-3">
-                <h1 className="text-sm font-bold">Rafael de Paula</h1>
-                <p className="flex ml-2 text-sm text-gray-500">
-                  @Rafael123 <span className="ml-1 -mt-1">.</span>
-                  <span className="ml-1">1h</span>
-                </p>
-              </div>
-            </div>
-            <IEtc className="mt-1" />
+      <section className="flex-col ml-3">
+        <header className="flex justify-between">
+          <div className="flex">
+            <h1 className="text-sm font-bold">Rafael de Paula</h1>
+            <p className="flex ml-2 text-sm text-gray-500">
+              @Rafael123 <span className="ml-1 -mt-1">.</span>
+              <span className="ml-1">1h</span>
+            </p>
           </div>
-        </section>
-      </article>
-    </section>
+          <IEtc className="mt-1" />
+        </header>
+
+        <div className="flex flex-col mt-1">
+          <h2 className="text-gray-800">
+            Tailwind v3.0 is hot off the press with a whole host of new features
+            and performance improvements, plus a beautiful new docs site 😍
+          </h2>
+          <div className="mt-2 border-2 border-gray-200 rounded-xl">
+            <img
+              src={PostImg}
+              className="rounded-t-xl border-b-2 border-gray-200 "
+              alt="Tailwind 3.0"
+            />
+            <div className="p-2">
+              <a
+                href="https://tailwindcss.com/"
+                className="text-sm text-gray-500"
+              >
+                tailwindcss.com
+              </a>
+              <h3 className="text-gray-800">
+                Tailwind CSS v3.0 - Tailwind CSS
+              </h3>
+              <p className="text-sm text-gray-500">
+                Tailwind CSS v3.0 is here — bringing incredible performance
+                gains, huge workflow improvements, and a seriously ridiculous
+                number of new features.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <footer>
+          <h1>footer</h1>
+        </footer>
+      </section>
+    </article>
   );
 };
 export default Tweet;
